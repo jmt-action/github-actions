340f64b69b36ddc4858b6815e56933f72c0f8f4f
sksrhd14 <jml3997@naver.com>
2025-02-12 22:20:37 +0900

    Add cache.yaml

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn GithubAction4
+          Learn GithubAction2
         </a>
       </header>
     </div>
